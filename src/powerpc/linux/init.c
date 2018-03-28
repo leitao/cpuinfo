4ce0cabb0a12268cd6760f8b0bb6a293ce122219
Rogerio Alves <rcardoso@linux.vnet.ibm.com>
2018-03-28 14:29:21 -0500

    Fix indentation.
    This commit fix indentation. I'm not sure if the mantainer follows any code
    standard but all code is uses Tab to indent.
    
    Signed-off-by: Rogerio Alves <rcardoso@linux.vnet.ibm.com>

--- src/powerpc/linux/init.c
+++ src/powerpc/linux/init.c
@@ -15,7 +15,6 @@ struct cpuinfo_powerpc_isa cpuinfo_isa = { 0 };
 
 static struct cpuinfo_package package = { { 0 } };
 
-
 static inline uint32_t min(uint32_t a, uint32_t b) {
 	return a < b ? a : b;
 }
@@ -25,9 +24,8 @@ static inline int cmp(uint32_t a, uint32_t b) {
 }
 
 static inline bool bitmask_all(uint32_t bitfield, uint32_t mask) {
-        return (bitfield & mask) == mask;
+	return (bitfield & mask) == mask;
 }
-
 
 void cpuinfo_powerpc_linux_init(void) {
 	struct cpuinfo_powerpc_linux_processor* powerpc_linux_processors = NULL;
@@ -39,11 +37,10 @@ void cpuinfo_powerpc_linux_init(void) {
 	struct cpuinfo_cache* l1i = NULL;
 	struct cpuinfo_cache* l1d = NULL;
 	struct cpuinfo_cache* l2 = NULL;
-        struct cpuinfo_cache* l3 = NULL;
-        struct cpuinfo_cache* l4 = NULL;
+	struct cpuinfo_cache* l3 = NULL;
+	struct cpuinfo_cache* l4 = NULL;
 	uint32_t usable_processors = 0;
 	struct cpuinfo_package* packages = NULL;
-
 
 	const uint32_t max_processors_count = cpuinfo_linux_get_max_processors_count();
 	cpuinfo_log_debug("system maximum processors count: %"PRIu32, max_processors_count);
@@ -98,13 +95,12 @@ void cpuinfo_powerpc_linux_init(void) {
 		powerpc_linux_processors[i].package_leader_id = i;
 	}
 
-        for (uint32_t i = 0; i < powerpc_linux_processors_count; i++) {
-                powerpc_linux_processors[i].system_processor_id = i;
-                if (bitmask_all(powerpc_linux_processors[i].flags, CPUINFO_LINUX_MASK_USABLE)) {
-                        usable_processors += 1;
+	for (uint32_t i = 0; i < powerpc_linux_processors_count; i++) {
+		powerpc_linux_processors[i].system_processor_id = i;
+		if (bitmask_all(powerpc_linux_processors[i].flags, CPUINFO_LINUX_MASK_USABLE)) {
+			usable_processors += 1;
 		}
 	}
-
 
 	processors = calloc(usable_processors, sizeof(struct cpuinfo_processor));
 	if (processors == NULL) {
@@ -120,70 +116,69 @@ void cpuinfo_powerpc_linux_init(void) {
 		goto cleanup;
 	}
 
-        /* TBD */
-        uint32_t packages_count = 10; 
-        clusters = calloc(packages_count, sizeof(struct cpuinfo_cluster));
-        if (clusters == NULL) {
-                cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" core clusters",
-                        packages_count * sizeof(struct cpuinfo_cluster), packages_count);
-                goto cleanup;
-        }
+	/* TBD */
+	uint32_t packages_count = 10;
+	clusters = calloc(packages_count, sizeof(struct cpuinfo_cluster));
+	if (clusters == NULL) {
+		cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" core clusters",
+			packages_count * sizeof(struct cpuinfo_cluster), packages_count);
+			goto cleanup;
+	}
 
-        /* TBD */
-        uint32_t l1i_count = 10; 
-        if (l1i_count != 0) {
-                l1i = calloc(l1i_count, sizeof(struct cpuinfo_cache));
-                if (l1i == NULL) {
-                        cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" L1I caches",
-                                l1i_count * sizeof(struct cpuinfo_cache), l1i_count);
-                        goto cleanup;
-                }
-        }
+	/* TBD */
+	uint32_t l1i_count = 10;
+	if (l1i_count != 0) {
+		l1i = calloc(l1i_count, sizeof(struct cpuinfo_cache));
+		if (l1i == NULL) {
+			cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" L1I caches",
+			l1i_count * sizeof(struct cpuinfo_cache), l1i_count);
+			goto cleanup;
+		}
+	}
 
-        /* TBD */
-        uint32_t l1d_count = 10; 
-        if (l1d_count != 0) {
-                l1d = calloc(l1d_count, sizeof(struct cpuinfo_cache));
-                if (l1d == NULL) {
-                        cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" L1D caches",
-                                l1d_count * sizeof(struct cpuinfo_cache), l1d_count);
-                        goto cleanup;
-                }
-        }
+	/* TBD */
+	uint32_t l1d_count = 10;
+	if (l1d_count != 0) {
+		l1d = calloc(l1d_count, sizeof(struct cpuinfo_cache));
+		if (l1d == NULL) {
+			cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" L1D caches",
+				l1d_count * sizeof(struct cpuinfo_cache), l1d_count);
+				goto cleanup;
+		}
+	}
 
-        /* TBD */
-        uint32_t l2_count = 10; 
-        if (l2_count != 0) {
-                l2 = calloc(l2_count, sizeof(struct cpuinfo_cache));
-                if (l2 == NULL) {
-                        cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" L2 caches",
-                                l2_count * sizeof(struct cpuinfo_cache), l2_count);
-                        goto cleanup;
-                }
-        }
+	/* TBD */
+	uint32_t l2_count = 10;
+	if (l2_count != 0) {
+		l2 = calloc(l2_count, sizeof(struct cpuinfo_cache));
+		if (l2 == NULL) {
+			cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" L2 caches",
+				l2_count * sizeof(struct cpuinfo_cache), l2_count);
+				goto cleanup;
+		}
+	}
 
-        /* TBD */
-        uint32_t l3_count = 10; 
-        if (l3_count != 0) {
-                l3 = calloc(l3_count, sizeof(struct cpuinfo_cache));
-                if (l3 == NULL) {
-                        cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" L3 caches",
-                                l3_count * sizeof(struct cpuinfo_cache), l3_count);
-                        goto cleanup;
-                }
-        }
+	/* TBD */
+	uint32_t l3_count = 10;
+	if (l3_count != 0) {
+		l3 = calloc(l3_count, sizeof(struct cpuinfo_cache));
+		if (l3 == NULL) {
+			cpuinfo_log_error("failed to allocate %zu bytes for descriptions of %"PRIu32" L3 caches",
+				l3_count * sizeof(struct cpuinfo_cache), l3_count);
+				goto cleanup;
+		}
+	}
 
-        /* TBD */
-        linux_cpu_to_core_map = calloc(powerpc_linux_processors_count, sizeof(struct cpuinfo_core*));
-        if (linux_cpu_to_core_map == NULL) {
-                cpuinfo_log_error("failed to allocate %zu bytes for mapping entries of %"PRIu32" cores",
-                        powerpc_linux_processors_count * sizeof(struct cpuinfo_core*),
-                        powerpc_linux_processors_count);
-                goto cleanup;
-        }
+	/* TBD */
+	linux_cpu_to_core_map = calloc(powerpc_linux_processors_count, sizeof(struct cpuinfo_core*));
+	if (linux_cpu_to_core_map == NULL) {
+		cpuinfo_log_error("failed to allocate %zu bytes for mapping entries of %"PRIu32" cores",
+		powerpc_linux_processors_count * sizeof(struct cpuinfo_core*),
+		powerpc_linux_processors_count);
+		goto cleanup;
+	}
 
-
-        /* TBD */
+	/* TBD */
 	linux_cpu_to_processor_map = calloc(powerpc_linux_processors_count, sizeof(struct cpuinfo_processor*));
 	if (linux_cpu_to_processor_map == NULL) {
 		cpuinfo_log_error("failed to allocate %zu bytes for %"PRIu32" logical processor mapping entries",
@@ -193,40 +188,39 @@ void cpuinfo_powerpc_linux_init(void) {
 
 	for (uint32_t i = 0; i < usable_processors; i++) {
 		processors[i].smt_id = 0; // TBD
-                processors[i].core = cores + i;
-                processors[i].cluster = clusters + 1; //TBD
-                processors[i].package = &package;
-                processors[i].linux_id = (int) powerpc_linux_processors[i].system_processor_id;
-                processors[i].cache.l1i = l1i + i;
-                processors[i].cache.l1d = l1d + i;
-                processors[i].cache.l2 = l2 ;
+		processors[i].core = cores + i;
+		processors[i].cluster = clusters + 1; // TBD
+		processors[i].package = &package;
+		processors[i].linux_id = (int) powerpc_linux_processors[i].system_processor_id;
+		processors[i].cache.l1i = l1i + i;
+		processors[i].cache.l1d = l1d + i;
+		processors[i].cache.l2 = l2 ;
 		cores[i].processor_start = i;
-                cores[i].processor_count = 1;
-                cores[i].core_id = i;
-                cores[i].cluster = clusters;
-                cores[i].package = &package;
-                cores[i].vendor = powerpc_linux_processors[i].vendor;
-                cores[i].uarch = powerpc_linux_processors[i].uarch;
-				// Disable all by default
-				cores[i].disabled = powerpc_linux_processors[i].disabled;
+		cores[i].processor_count = 1;
+		cores[i].core_id = i;
+		cores[i].cluster = clusters;
+		cores[i].package = &package;
+		cores[i].vendor = powerpc_linux_processors[i].vendor;
+		cores[i].uarch = powerpc_linux_processors[i].uarch;
+		/* Disable all by default */
+		cores[i].disabled = powerpc_linux_processors[i].disabled;
 	}
 
-
-     // Packages
-    packages = calloc(4, sizeof(struct cpuinfo_package));
+	/* Packages */
+	packages = calloc(4, sizeof(struct cpuinfo_package));
 	if (packages == NULL) {
-			goto cleanup;
+		goto cleanup;
 	}
 	strcpy(&packages[0].name, "POWER8 Single core");
 	cpuinfo_packages = packages;
 	cpuinfo_packages_count = 1;
 
-
+	/* Commit */
 	cpuinfo_linux_cpu_to_processor_map = linux_cpu_to_processor_map;
 	cpuinfo_linux_cpu_to_core_map = linux_cpu_to_core_map;
 	cpuinfo_processors = processors;
 	cpuinfo_cores = cores;
-	cpuinfo_packages = packages; 
+	cpuinfo_packages = packages;
 	cpuinfo_clusters = clusters;
 	//cpuinfo_packages = &package;
 	//cpuinfo_cache[cpuinfo_cache_level_1i] = l1i;
@@ -254,8 +248,8 @@ cleanup:
 	free(linux_cpu_to_core_map);
 	free(processors);
 	free(cores);
-        free(l1i);
-        free(l1d);
-        free(l2);
-        free(l3);
+	free(l1i);
+	free(l1d);
+	free(l2);
+	free(l3);
 }
