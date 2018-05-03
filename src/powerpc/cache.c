af1a3989a719921a3da00298af528076c09417bf
Rogerio Alves <rcardoso@linux.vnet.ibm.com>
2018-05-03 07:26:34 -0500

    Fix indentation
    Fix indentation on cache.c. Replace spaces by tab and remove trailing
    whitespaces.
    
    Signed-off-by: Rogerio Alves <rcardoso@linux.vnet.ibm.com>

--- src/powerpc/cache.c
+++ src/powerpc/cache.c
@@ -16,7 +16,7 @@
 
 int path_exist(const char *path)
 {
-    return (access(path, F_OK) == 0);
+	return (access(path, F_OK) == 0);
 }
 
 void read_str(char *result, size_t len, const char *path)
@@ -26,7 +26,7 @@ void read_str(char *result, size_t len, const char *path)
 	f = fopen(path, "r");
 
 	fgets(result, len, f);
-        fclose(f);
+	fclose(f);
 
 	len = strlen(result);
 	if (result[len - 1] == '\n')
@@ -34,80 +34,83 @@ void read_str(char *result, size_t len, const char *path)
 }
 
 void cpuinfo_powerpc_decode_cache(
-        uint32_t smt_id,
-        struct cpuinfo_cache l1i[restrict static 1],
-        struct cpuinfo_cache l1d[restrict static 1],
-        struct cpuinfo_cache l2[restrict static 1],
-        struct cpuinfo_cache l3[restrict static 1])
+	uint32_t smt_id,
+	struct cpuinfo_cache l1i[restrict static 1],
+	struct cpuinfo_cache l1d[restrict static 1],
+	struct cpuinfo_cache l2[restrict static 1],
+	struct cpuinfo_cache l3[restrict static 1])
 {
 	char buf[BUF_SIZE];
 	char result[BUF_SIZE];
-        uint32_t i;
-        uint32_t size = 0, associativity = 0, line_size = 0, sets = 0; 
-        size_t len;
+	uint32_t i;
+	uint32_t size = 0, associativity = 0, line_size = 0, sets = 0;
+	size_t len;
 
 	for(i = 0; i < NUM_CACHE; i++) {
-           sprintf (buf, "/sys/devices/system/cpu/cpu%d/cache/index%d/size", smt_id, i);
-           if(path_exist(buf))
-               read_str(result, sizeof(result), buf);
-               len = strlen(result);
-	           if (result[len - 1] == 'K')
-		       result[len - 1] = '\0';
+		sprintf (buf, "/sys/devices/system/cpu/cpu%d/cache/index%d/size", smt_id, i);
 
-           size = atoi(result);
+		if(path_exist(buf))
+			read_str(result, sizeof(result), buf);
 
-           sprintf (buf, "/sys/devices/system/cpu/cpu%d/cache/index%d/ways_of_associativity", smt_id, i);
-           if(path_exist(buf))
-               read_str(result, sizeof(result), buf);
+		len = strlen(result);
 
-           associativity = atoi(result);
+		if (result[len - 1] == 'K')
+			result[len - 1] = '\0';
 
-           sprintf (buf, "/sys/devices/system/cpu/cpu%d/cache/index%d/coherency_line_size", smt_id, i);
-           if(path_exist(buf))
-               read_str(result, sizeof(result), buf);
+		size = atoi(result);
+		sprintf (buf, "/sys/devices/system/cpu/cpu%d/cache/index%d/ways_of_associativity", smt_id, i);
 
-           line_size = atoi(result);
+		if(path_exist(buf))
+			read_str(result, sizeof(result), buf);
 
-           sprintf (buf, "/sys/devices/system/cpu/cpu%d/cache/index%d/number_of_sets", smt_id, i);
-           if(path_exist(buf))
-               read_str(result, sizeof(result), buf);
+		associativity = atoi(result);
+		sprintf (buf, "/sys/devices/system/cpu/cpu%d/cache/index%d/coherency_line_size", smt_id, i);
 
-           sets = atoi(result);
-		
-           switch (i) {
-               case 0:
-                   *l1d = (struct cpuinfo_cache) {
-                       .size = size * 1024,
-                       .associativity = associativity,
-                       .line_size = line_size,
-                       .sets = sets 
-                   };
-                   break;
-               case 1:
-                   *l1i = (struct cpuinfo_cache) {
-                       .size = size * 1024,
-                       .associativity = associativity,
-                       .line_size = line_size,
-                       .sets = sets
-                   };
-                   break;
-               case 2:
-                   *l2 = (struct cpuinfo_cache) {
-                       .size = size * 1024,
-                       .associativity = 0,
-                       .line_size = 0,
-                       .sets = sets 
-                   };
-                   break;
-               case 3:
-                   *l3 = (struct cpuinfo_cache) {
-                       .size = size * 1024,
-                       .associativity = 0,
-                       .line_size = 0,
-                       .sets = sets
-                   };
-                   break;
-           }
+		if(path_exist(buf))
+		read_str(result, sizeof(result), buf);
 
-	} 
+		line_size = atoi(result);
+
+		sprintf (buf, "/sys/devices/system/cpu/cpu%d/cache/index%d/number_of_sets", smt_id, i);
+		if(path_exist(buf))
+			read_str(result, sizeof(result), buf);
+
+		sets = atoi(result);
+
+		switch (i) {
+			case 0:
+				*l1d = (struct cpuinfo_cache) {
+						.size = size * 1024,
+						.associativity = associativity,
+						.line_size = line_size,
+						.sets = sets
+				};
+				break;
+			case 1:
+				*l1i = (struct cpuinfo_cache) {
+						.size = size * 1024,
+						.associativity = associativity,
+						.line_size = line_size,
+						.sets = sets
+				};
+				break;
+			case 2:
+				*l2 = (struct cpuinfo_cache) {
+						.size = size * 1024,
+						.associativity = 0,
+						.line_size = 0,
+						.sets = sets
+				};
+				break;
+			case 3:
+				*l3 = (struct cpuinfo_cache) {
+						.size = size * 1024,
+						.associativity = 0,
+						.line_size = 0,
+						.sets = sets
+				};
+				break;
+
+		}
+	}
 }
